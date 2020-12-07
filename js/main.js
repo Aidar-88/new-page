179ba41bec87ddfaa5e650082c9e3b842abb365d
Aidar-88 <75580273+Aidar-88@users.noreply.github.com>
2020-12-07 15:03:34 +0600

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function(){
+    $('.slider_inner').slick({
+        nextArrow: '<button type="button" class="slick_btn slick_next"></button>',
+        prevArrow: '<button type="button" class="slick_btn slick_prev"></button>',
+        infinite: false
+    });
+
+
+}
+);
